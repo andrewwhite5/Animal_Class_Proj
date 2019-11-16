84adaab6b972b8b6fac0b3a2aeaba69714ff309a
andrewwhite5 <andrewwhite5@users.noreply.github.com>
2019-11-15 19:42:40 -0700

    Third

--- tests/tests_pytest.py
+++ tests/tests_pytest.py
@@ -1,28 +1,55 @@
 import pytest
+import glob
+import os
+import json
+
 from src.dog import Dog
+
 
 @pytest.fixture()
 def setup():
     dog = Dog(age=0.5, weight=10)
-    yield dog
+    f_path = dog.save_to_json(out_path='/tmp')
+    yield dog, f_path
+    [os.remove(f_path) for f_path in glob.glob('/tmp/dog_*.json')]
+
 
 def test_is_puppy(setup):
-    dog = setup
+    dog, _ = setup
     assert dog.is_puppy() == True
+
 
 def test_num_legs():
     assert Dog.num_legs == 4
 
+
 def test_add_one_year(setup):
-    dog = setup
+    dog, _ = setup
     dog.add_one_year()
     assert dog.age == 1.5
 
+
 def test_not_is_puppy(setup):
-    dog = setup
+    dog, _ = setup
     dog.add_one_year()
     assert dog.is_puppy() is False
+
 
 def test_fail_invalid_age():
     with pytest.raises(ValueError):
         Dog(age=-1, weight=10)
+
+
+def test_save_to_json(setup):
+    dog, f_path = setup
+    with open(f_path) as f:
+        json_data = json.load(f)
+    assert json_data['age'] == 0.5
+    assert json_data['weight'] == 10
+
+
+def test_load_from_json(setup):
+    dog, _ = setup
+    f_list = glob.glob('/tmp/*.json')
+    assert len(f_list) == 1
+    dog = Dog.load_from_json(f_list[0])
--- tests/tests_unittest.py
+++ tests/tests_unittest.py
@@ -2,6 +2,7 @@ import unittest
 import random
 from src.animal import Animal
 
+
 # You MUST make sure that the tests can run independently. If one will cause
 #   another to fail upon you changing the order, you've done it wrong.
 class TestAnimal(unittest.TestCase):
@@ -9,15 +10,19 @@ class TestAnimal(unittest.TestCase):
         self.test_age = random.randint(0, 100)
         self.animal = Animal(age=self.test_age)
 
+
     def test_repr(self):
         self.assertEqual(repr(self.animal), f'Animal(age={self.test_age})')
+
 
     def test_str(self):
         self.assertEqual(str(
             self.animal), f'This is an animal that is {self.test_age} years old.')
 
+
     def test_age_next_year(self):
         self.assertEqual(self.animal.age_next_year(), self.test_age + 1)
+
 
     def test_add_one_year(self):
         self.animal.add_one_year()
